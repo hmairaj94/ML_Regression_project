2bad1bc9a5f8deec5a5aa7532302021f135991f0
hmairaj94 <hmairaj94@gmail.com>
2024-09-04 01:19:34 +0530

    Model training

--- Banglore_property_price/Sever/util.py
+++ Banglore_property_price/Sever/util.py
@@ -30,13 +30,13 @@ def load_saved_artifacts():
     global __locations
     global __data_columns
 
-    with open('Classification/Sever/artifacts/columns.json','r') as f:
+    with open('Banglore_property_price/Sever/artifacts/columns.json','r') as f:
        __data_columns =  json.load(f)['data_columns']
        __locations = __data_columns[3:]
 
        global __model
 
-    with open("Classification/Sever/artifacts/banglore_home_prices_model.pickle",'rb') as f:
+    with open("Banglore_property_price/Sever/artifacts/banglore_home_prices_model.pickle",'rb') as f:
         __model = pickle.load(f)
         print("loading saved artifacts...done")
     
